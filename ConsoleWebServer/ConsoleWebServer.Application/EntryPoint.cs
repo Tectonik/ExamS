e3b9cd19aa0dfb7c23af7e4f7c90ef47a7c7c1e7
Ro6afF <dimo_chanev@windowslive.com>
2015-10-15 16:19:18 +0300

    Test  commit

---
+++ ConsoleWebServer/ConsoleWebServer.Application/EntryPoint.cs
@@ -0,0 +1,10 @@
+﻿// TODO: Describe patterns, SOLID, bugs and bottleneck in Documentation.txt
+
+public static class EntryPoint
+{
+    public static void Main(int a)
+    {
+        var webSever = new WebServerConsole5();
+        webSever.Start();
+    }
+};
